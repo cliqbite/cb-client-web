dbc132cfda3f20b6cdf3d8cf3c74d26876860b34
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2023-10-02 18:01:50 +0530

    chore: development setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/demo',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
