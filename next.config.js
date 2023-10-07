72c931a04dfb4694fae5bf83b615a0bf6edae260
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2023-10-07 23:22:48 +0530

    feat(pwa): add pwa with basic manifest
    added pwa package and basic manifest file generated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
+const withPWA = require('next-pwa')
 
 const nextConfig = {
+  ...withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    // disable: process.env.NODE_ENV === 'development',
+  }),
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
