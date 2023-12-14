00a737a7271509f0f008920041cc5122ec28d0a5
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2023-12-14 09:13:38 +0530

    feat(auth): redirect if old user

--- src/app/(auth)/college/view.tsx
+++ src/app/(auth)/college/view.tsx
@@ -1,26 +1,34 @@
 'use client'
-import { cls } from '@/common/utils/classnames'
-import styles from './page.module.scss'
-import Select from '@/components/ui/selection/select'
-import { motion } from 'framer-motion'
-import Button from '@/components/ui/button'
-import profileService from '@/client/modules/profile'
-import { ROUTE } from '@/common/constants/route'
-import { useState, type ChangeEvent, useEffect } from 'react'
-import { log } from '@/common/log'
-import { useQuery } from '@tanstack/react-query'
-import { getCollege } from '@/client/functions/getCollege'
-import Loader from '@/components/ui/loader'
-import { useRouter } from 'next/navigation'
-import { Canteen } from '@/server/model/canteen'
 import { getCanteen } from '@/client/functions/getCateen'
-import { College } from '@/server/model/college'
-import { Colleges } from '@/common/types'
+import { getCollege } from '@/client/functions/getCollege'
+import { getUser } from '@/client/functions/getUser'
+import profileService from '@/client/modules/profile'
 import { useCanteen } from '@/client/store/useCanteen'
 import { useCollege } from '@/client/store/useCollege'
+import { ROUTE } from '@/common/constants/route'
+import { log } from '@/common/log'
+import { Colleges } from '@/common/types'
+import Button from '@/components/ui/button'
+import Loader from '@/components/ui/loader'
+import Select from '@/components/ui/selection/select'
+import { Canteen } from '@/server/model/canteen'
+import { College } from '@/server/model/college'
+import { useQuery } from '@tanstack/react-query'
+import { motion } from 'framer-motion'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState, type ChangeEvent } from 'react'
 
 const View = () => {
   const router = useRouter()
+  const {
+    data: user,
+    isLoading: isUserLoading,
+    isSuccess: isUserFetched
+  } = useQuery({
+    queryKey: ['user'],
+    queryFn: getUser
+  })
+
   const {
     data: colleges,
     isLoading: isCollegeLoading,
@@ -44,6 +52,19 @@ const View = () => {
   const setCollege = useCollege((value) => value.setCollege)
   const setCanteens = useCanteen((value) => value.setCanteens)
   const setCanteen = useCanteen((value) => value.setCanteen)
+
+  if (!isUserLoading && isUserFetched) {
+    const userPerfs = user?.prefs || []
+    if (userPerfs) {
+      const mappedUserPrefs = new Map(Object.entries(userPerfs))
+      log('mappedUserPrefs::', mappedUserPrefs)
+      if (mappedUserPrefs.has('college') && mappedUserPrefs.has('canteen')) {
+        setCanteen(mappedUserPrefs.get('canteen'))
+        setCollege(mappedUserPrefs.get('college'))
+        router.push(ROUTE.HOME)
+      }
+    }
+  }
 
   if (isClgSuccess && colleges.length) {
     setColleges(colleges)
@@ -100,49 +121,59 @@ const View = () => {
 
   return (
     <section>
-      <motion.div
-        initial={{ x: '-50%', opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.3, ease: 'easeInOut' }}
-      >
-        <h2 className='text-base font-semibold leading-7 text-gray-900'>
-          Select your college and canteen
-        </h2>
-        {isCollegeLoading ? (
+      {isUserLoading ? (
+        <>
           <Loader>
-            <Loader.Dot />
+            <Loader.Default />
           </Loader>
-        ) : (
-          <>
-            <Select onChange={onOptionChangeHandler} id='college'>
-              <Select.OptionClg options={colleges as Colleges} />
-            </Select>
-            <br />
-            {isCanteenLoading ? (
+        </>
+      ) : (
+        <>
+          <motion.div
+            initial={{ x: '-50%', opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.3, ease: 'easeInOut' }}
+          >
+            <h2 className='text-base font-semibold leading-7 text-gray-900'>
+              Select your college and canteen
+            </h2>
+            {isCollegeLoading ? (
               <Loader>
                 <Loader.Dot />
               </Loader>
             ) : (
-              <Select
-                onChange={onOptionChangeHandler}
-                id='canteen'
-                disabled={!(status === 'success' && fetchStatus === 'idle')}
-              >
-                <Select.OptionCanteen options={canteens as Canteen[]} />
-              </Select>
+              <>
+                <Select onChange={onOptionChangeHandler} id='college'>
+                  <Select.OptionClg options={colleges as Colleges} />
+                </Select>
+                <br />
+                {isCanteenLoading ? (
+                  <Loader>
+                    <Loader.Dot />
+                  </Loader>
+                ) : (
+                  <Select
+                    onChange={onOptionChangeHandler}
+                    id='canteen'
+                    disabled={!(status === 'success' && fetchStatus === 'idle')}
+                  >
+                    <Select.OptionCanteen options={canteens as Canteen[]} />
+                  </Select>
+                )}
+                <br />
+                <Button
+                  disabled={!(state.college.id && state.canteen.id)}
+                  btn='outline'
+                  className={'p-2 w-full'}
+                  onClick={updateCollegeCanteenPreference}
+                >
+                  Grab your order!
+                </Button>
+              </>
             )}
-            <br />
-            <Button
-              disabled={!(state.college.id && state.canteen.id)}
-              btn='outline'
-              className={'p-2 w-full'}
-              onClick={updateCollegeCanteenPreference}
-            >
-              Grab your order!
-            </Button>
-          </>
-        )}
-      </motion.div>
+          </motion.div>
+        </>
+      )}
     </section>
   )
 }
--- src/app/(auth)/success/view.tsx
+++ src/app/(auth)/success/view.tsx
@@ -1,28 +1,30 @@
 'use client'
-import Input from '@/components/ui/input/outline'
-import { useState, type FC, ChangeEvent } from 'react'
-import { z } from 'zod'
-import { useForm, SubmitHandler } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import styles from './page.module.scss'
-import Button from '@/components/ui/button'
-import { cls } from '@/common/utils/classnames'
-import profileService from '@/client/modules/profile'
-import { log } from '@/common/log'
-import { motion } from 'framer-motion'
-import type { College } from '@/common/types'
-import { useCollege } from '@/client/store/useCollege'
-import Link from 'next/link'
-import { ROUTE } from '@/common/constants/route'
-import Select from '@/components/ui/selection/select'
-import { DateFns } from '@/common/utils/date/format'
-import Icon from '@/components/ui/icon'
-import { useQuery } from '@tanstack/react-query'
-import { getCollege } from '@/client/functions/getCollege'
-import Loader from '@/components/ui/loader'
-import { Canteen } from '@/server/model/canteen'
 import { getCanteen } from '@/client/functions/getCateen'
+import { getCollege } from '@/client/functions/getCollege'
+import { getUser } from '@/client/functions/getUser'
+import profileService from '@/client/modules/profile'
 import { useCanteen } from '@/client/store/useCanteen'
+import { useCollege } from '@/client/store/useCollege'
+import { ROUTE } from '@/common/constants/route'
+import { log } from '@/common/log'
+import type { College } from '@/common/types'
+import { cls } from '@/common/utils/classnames'
+import { DateFns } from '@/common/utils/date/format'
+import Button from '@/components/ui/button'
+import Icon from '@/components/ui/icon'
+import Input from '@/components/ui/input/outline'
+import Loader from '@/components/ui/loader'
+import Select from '@/components/ui/selection/select'
+import { Canteen } from '@/server/model/canteen'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useQuery } from '@tanstack/react-query'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { ChangeEvent, useState, type FC } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import styles from './page.module.scss'
 
 interface ViewProps {}
 
@@ -86,6 +88,16 @@ const View: FC<ViewProps> = () => {
     retry: 3
   })
 
+  const router = useRouter()
+  const {
+    data: user,
+    isLoading: isUserLoading,
+    isSuccess: isUserFetched
+  } = useQuery({
+    queryKey: ['user'],
+    queryFn: getUser
+  })
+
   const [previousStep, setPreviousStep] = useState(0)
   const [currentStep, setCurrentStep] = useState(0)
   const [selectedClg, setSelectedClg] = useState({} as College)
@@ -105,6 +117,20 @@ const View: FC<ViewProps> = () => {
   const setCollege = useCollege((state) => state.setCollege)
   const setCanteen = useCanteen((state) => state.setCanteen)
   const setCanteens = useCanteen((state) => state.setCanteens)
+
+  if (!isUserLoading && isUserFetched) {
+    const userPerfs = user?.prefs || []
+    if (userPerfs) {
+      const mappedUserPrefs = new Map(Object.entries(userPerfs))
+      log('mappedUserPrefs::', mappedUserPrefs)
+      if (mappedUserPrefs.has('college') && mappedUserPrefs.has('canteen')) {
+        setCanteen(mappedUserPrefs.get('canteen'))
+        setCollege(mappedUserPrefs.get('college'))
+        router.push(ROUTE.HOME)
+      }
+    }
+  }
+
   isClgSuccess && setColleges(colleges)
 
   const handleClgOnChange = (event: ChangeEvent<HTMLSelectElement>) => {
@@ -178,152 +204,160 @@ const View: FC<ViewProps> = () => {
 
   return (
     <section className={styles.step}>
-      <>
-        <form onSubmit={handleSubmit(processForm)}>
-          {currentStep === 0 && (
-            <section className={cls(styles['step-1'])}>
-              <span>
-                <h1>Awesome</h1>
-                <Icon icon='awesome' size={32} className={styles.icon} />
-              </span>
-              <strong> Your are successfully logged-in</strong>
-              <p>You are now part of CliqBite.</p>
-            </section>
-          )}
-          {currentStep === 1 && (
-            <motion.div
-              initial={{ x: delta >= 0 ? '50%' : '-50%', opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 0.3, ease: 'easeInOut' }}
-            >
-              <h2 className='text-base font-semibold leading-7 text-gray-900'>
-                Personal Information
-              </h2>
+      {isUserLoading ? (
+        <>
+          <Loader>
+            <Loader.Default />
+          </Loader>
+        </>
+      ) : (
+        <>
+          <form onSubmit={handleSubmit(processForm)}>
+            {currentStep === 0 && (
+              <section className={cls(styles['step-1'])}>
+                <span>
+                  <h1>Awesome</h1>
+                  <Icon icon='awesome' size={32} className={styles.icon} />
+                </span>
+                <strong> Your are successfully logged-in</strong>
+                <p>You are now part of CliqBite.</p>
+              </section>
+            )}
+            {currentStep === 1 && (
+              <motion.div
+                initial={{ x: delta >= 0 ? '50%' : '-50%', opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.3, ease: 'easeInOut' }}
+              >
+                <h2 className='text-base font-semibold leading-7 text-gray-900'>
+                  Personal Information
+                </h2>
 
-              <Input
-                type='text'
-                {...register('firstName')}
-                placeholder={''}
-                error={errors.firstName?.message}
-                label='Name'
-                className={styles.cta}
-                outlineclassName={styles.cta}
-                wapperclassName={styles.cta}
-                autoComplete='given-name'
-              />
-              <Input
-                {...register('lastName')}
-                placeholder={''}
-                error={errors.lastName?.message}
-                label='Last name'
-                className={styles.cta}
-                outlineclassName={styles.cta}
-                wapperclassName={styles.cta}
-                autoComplete='family-name'
-              />
+                <Input
+                  type='text'
+                  {...register('firstName')}
+                  placeholder={''}
+                  error={errors.firstName?.message}
+                  label='Name'
+                  className={styles.cta}
+                  outlineclassName={styles.cta}
+                  wapperclassName={styles.cta}
+                  autoComplete='given-name'
+                />
+                <Input
+                  {...register('lastName')}
+                  placeholder={''}
+                  error={errors.lastName?.message}
+                  label='Last name'
+                  className={styles.cta}
+                  outlineclassName={styles.cta}
+                  wapperclassName={styles.cta}
+                  autoComplete='family-name'
+                />
 
-              <Input
-                {...register('dateOfBirth')}
-                placeholder={''}
-                error={errors.dateOfBirth?.message}
-                label='Date of Birth'
-                type='date'
-                className={styles.cta}
-                outlineclassName={styles.cta}
-                wapperclassName={styles.cta}
-                autoComplete='bday'
-              />
-            </motion.div>
-          )}
+                <Input
+                  {...register('dateOfBirth')}
+                  placeholder={''}
+                  error={errors.dateOfBirth?.message}
+                  label='Date of Birth'
+                  type='date'
+                  className={styles.cta}
+                  outlineclassName={styles.cta}
+                  wapperclassName={styles.cta}
+                  autoComplete='bday'
+                />
+              </motion.div>
+            )}
 
-          {currentStep === 2 && (
-            <motion.div
-              initial={{ x: delta >= 0 ? '50%' : '-50%', opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 0.3, ease: 'easeInOut' }}
-            >
-              <h2 className='text-base font-semibold leading-7 text-gray-900'>
-                Select your college
-              </h2>
-              {isCollegeLoading ? (
-                <Loader>
-                  <Loader.Dot />
-                </Loader>
-              ) : (
-                isClgSuccess && (
-                  <Select
-                    register={register('college', {
-                      onChange: handleClgOnChange
-                    })}
-                  >
-                    <Select.OptionClg options={colleges} />
-                  </Select>
-                )
-              )}
-              <br />
-              {isCanteenLoading && status !== 'success' ? (
-                <Loader>
-                  <Loader.Dot />
-                </Loader>
-              ) : (
-                <Select
-                  register={register('canteen')}
-                  disabled={!(status === 'success' && fetchStatus === 'idle')}
-                >
-                  <Select.OptionCanteen options={canteens as Canteen[]} />
-                </Select>
-              )}
-              <br />
-            </motion.div>
-          )}
-
-          {currentStep === 3 && (
-            <>
-              <h2 className='text-base font-semibold leading-7 text-gray-900'>
-                Complete
-              </h2>
-              <p className='mt-1 text-sm leading-6 text-gray-600'>
-                Thank you for your submission.
-              </p>
-            </>
-          )}
-        </form>
-        <section className={styles.action}>
-          {currentStep < steps.length - 1 ? (
-            <>
-              <Button
-                btn='base'
-                className={cls(
-                  styles.cta,
-                  styles.btn,
-                  currentStep === 0 && styles.hide
+            {currentStep === 2 && (
+              <motion.div
+                initial={{ x: delta >= 0 ? '50%' : '-50%', opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.3, ease: 'easeInOut' }}
+              >
+                <h2 className='text-base font-semibold leading-7 text-gray-900'>
+                  Select your college
+                </h2>
+                {isCollegeLoading ? (
+                  <Loader>
+                    <Loader.Dot />
+                  </Loader>
+                ) : (
+                  isClgSuccess && (
+                    <Select
+                      register={register('college', {
+                        onChange: handleClgOnChange
+                      })}
+                    >
+                      <Select.OptionClg options={colleges} />
+                    </Select>
+                  )
                 )}
-                disabled={isSubmitting}
-                onClick={prev}
-              >
-                <Icon icon='back' />
-              </Button>
-              <Button
-                btn='base'
+                <br />
+                {isCanteenLoading && status !== 'success' ? (
+                  <Loader>
+                    <Loader.Dot />
+                  </Loader>
+                ) : (
+                  <Select
+                    register={register('canteen')}
+                    disabled={!(status === 'success' && fetchStatus === 'idle')}
+                  >
+                    <Select.OptionCanteen options={canteens as Canteen[]} />
+                  </Select>
+                )}
+                <br />
+              </motion.div>
+            )}
+
+            {currentStep === 3 && (
+              <>
+                <h2 className='text-base font-semibold leading-7 text-gray-900'>
+                  Complete
+                </h2>
+                <p className='mt-1 text-sm leading-6 text-gray-600'>
+                  Thank you for your submission.
+                </p>
+              </>
+            )}
+          </form>
+          <section className={styles.action}>
+            {currentStep < steps.length - 1 ? (
+              <>
+                <Button
+                  btn='base'
+                  className={cls(
+                    styles.cta,
+                    styles.btn,
+                    currentStep === 0 && styles.hide
+                  )}
+                  disabled={isSubmitting}
+                  onClick={prev}
+                >
+                  <Icon icon='back' />
+                </Button>
+                <Button
+                  btn='base'
+                  className={cls(styles.cta, styles.btn)}
+                  disabled={isSubmitting}
+                  onClick={next}
+                >
+                  Next
+                  <Icon icon='back' style={{ rotate: '180deg' }} />
+                </Button>
+              </>
+            ) : (
+              <Link
+                href={ROUTE.HOME}
                 className={cls(styles.cta, styles.btn)}
-                disabled={isSubmitting}
-                onClick={next}
+                passHref
               >
-                Next
-                <Icon icon='back' style={{ rotate: '180deg' }} />
-              </Button>
-            </>
-          ) : (
-            <Link
-              href={ROUTE.HOME}
-              className={cls(styles.cta, styles.btn)}
-              passHref
-            >
-              <Button btn='outline'>Click! to Grab your Food</Button>
-            </Link>
-          )}
-        </section>
-      </>
+                <Button btn='outline'>Click! to Grab your Food</Button>
+              </Link>
+            )}
+          </section>
+        </>
+      )}
     </section>
   )
 }
