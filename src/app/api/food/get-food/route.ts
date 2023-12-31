fbcf3350a83e38d60e8d0a3a393bc4d3693fea2e
Karthikeya037 <karthikeyaalkeer@gmail.com>
2023-12-31 10:02:08 +0530

    feat(apis):api to get food list based on search strings

--- src/app/api/food/get-food/route.ts
+++ src/app/api/food/get-food/route.ts
@@ -3,15 +3,12 @@ import foodService from '@/app/utils/services/foodService'
 export async function GET(req: any) {
   try {
     const canteen_id = req.nextUrl.searchParams.get('canteen_id')
-    const category = req.nextUrl.searchParams.get('category')
+    const name = req.nextUrl.searchParams.get('name')
     if (!canteen_id) {
       throw new Error('Please select the canteen!')
     }
-    if (canteen_id && !category) {
-      let items = await foodService.getMenu(canteen_id)
-      return NextResponse.json(items)
-    } else if (canteen_id && category) {
-      let items = await foodService.getOrdersByCategory(canteen_id, category)
+    if (canteen_id && name) {
+      let items = await foodService.getFoods(name, canteen_id)
       return NextResponse.json(items)
     }
   } catch (error) {
